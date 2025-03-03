cc0257e8583eff594ad99660ebf3fdfd0c9e39eb
mcflonilo <lm.opheim39@gmail.com>
2025-03-03 11:58:48 +0100

    reee

--- nyttige_filer/bitbucket filer/ultradeep-ultrabend-ec886561e05a-2/src/Ultra_Bend.BSEngine/BSEngine/Output/Load.cs
+++
@@ -1,8 +0,0 @@
-namespace UltraBend.Services.BSEngine.Output
-{
-    public class Load
-    {
-        public double Force { get; set; }
-        public double RelativeForceAngle { get; set; }
-    }
-}
